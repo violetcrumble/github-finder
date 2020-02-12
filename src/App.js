343ffa78419e5029e5a0c611d7e45f71c123cc19
Bonnie Mellott <bonniemellott@gmail.com>
2020-02-12 08:50:36 -0500

    adding repo listing

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Users from "./components/users/Users";
 import User from "./components/users/User";
 import Search from "./components/users/Search";
 import Alert from "./components/layout/Alert";
-import About from './components/pages/About';
+import About from "./components/pages/About";
 import axios from "axios";
 import "./App.css";
 
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   };
@@ -39,7 +40,7 @@ class App extends Component {
   };
 
   // Get single Github user
-  getUser = async (username) => {
+  getUser = async username => {
     this.setState({ loading: true });
 
     const res = await axios.get(
@@ -50,7 +51,21 @@ class App extends Component {
       user: res.data,
       loading: false
     });
-  }
+  };
+
+  // Get users repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}
+      &client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    this.setState({
+      repos: res.data,
+      loading: false
+    });
+  };
 
   // Clear users from state
   clearUsers = () => {
@@ -67,7 +82,7 @@ class App extends Component {
   };
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
       <Router>
@@ -92,9 +107,20 @@ class App extends Component {
                 )}
               />
               <Route exact path="/about" component={About} />
-              <Route exact path="/user/:login" render={props => (
-                <User { ...props } getUser={this.getUser} user={user} loading={loading} />
-              )} />
+              <Route
+                exact
+                path="/user/:login"
+                render={props => (
+                  <User
+                    {...props}
+                    getUser={this.getUser}
+                    getUserRepos={this.getUserRepos}
+                    user={user}
+                    repos={repos}
+                    loading={loading}
+                  />
+                )}
+              />
             </Switch>
           </div>
         </div>
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,6 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Navbar = ({ icon, title }) => {
   return (
@@ -9,8 +9,12 @@ const Navbar = ({ icon, title }) => {
         <i className={icon}></i> {title}
       </h1>
       <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
       </ul>
     </nav>
   );
